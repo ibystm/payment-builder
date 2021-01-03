451d2cd278a1240334beb7d1b1ad23f9189158d3
Kosuke Takahashi <rhcp.kdog@gmail.com>
2021-01-03 15:47:22 +0900

    delete root app.js

--- functions/src/app.ts
+++ functions/src/app.ts
@@ -1,49 +1,49 @@
-import * as express from "express";
-import * as cookieParser from "cookie-parser";
-import index from "./api/v1/index";
-import * as createError from "http-errors";
+// import * as express from "express";
+// import * as cookieParser from "cookie-parser";
+// import index from "./api/v1/index";
+// import * as createError from "http-errors";
 
-const app = express();
+// const app = express();
 
-// view engine setup
-app.set("view engine", "jade");
+// // view engine setup
+// app.set("view engine", "jade");
 
-/**
- *  ミドルウェアのマウント
- *  express.json()はjsonを扱えるようにしている
- *  express.urlencoded(): URL解析
- * */
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-// ヘッダー情報からcookie情報を拾ってくれる
-app.use(cookieParser());
+// /**
+//  *  ミドルウェアのマウント
+//  *  express.json()はjsonを扱えるようにしている
+//  *  express.urlencoded(): URL解析
+//  * */
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// // ヘッダー情報からcookie情報を拾ってくれる
+// app.use(cookieParser());
 
-app.use("/api/v1", index);
+// app.use("/api/v1", index);
 
-// catch 404 and forward to error handler
-app.use(function (
-  req: express.Request,
-  res: express.Response,
-  next: express.NextFunction
-) {
-  // errorオブジェクトを作ってくれるミドルウェア
-  next(createError(404));
-});
+// // catch 404 and forward to error handler
+// app.use(function (
+//   req: express.Request,
+//   res: express.Response,
+//   next: express.NextFunction
+// ) {
+//   // errorオブジェクトを作ってくれるミドルウェア
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function (
-  err: any,
-  req: express.Request,
-  res: express.Response,
-  next: express.NextFunction
-) {
-  // µset locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+// // error handler
+// app.use(function (
+//   err: any,
+//   req: express.Request,
+//   res: express.Response,
+//   next: express.NextFunction
+// ) {
+//   // µset locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render("error");
+// });
 
-export default app;
+// export default app;
